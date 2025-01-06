a149144e186eb1cc4027c2df15b6bf1c87353aee
shibil-thora <shibilallakkat123@gmail.com>
2025-01-06 09:59:10 +0530

    completed Error handling part

--- error_handling.py
+++ error_handling.py
@@ -1,2 +1,22 @@
 import asyncio 
 
+async def might_fail() :
+    try: 
+        await asyncio.sleep(3)
+        print("Success")
+    except asyncio.CancelledError: 
+        print("Operation cancelled")
+    
+
+async def main(): 
+    task = asyncio.create_task(might_fail())
+
+    try: 
+        await asyncio.wait_for(task, timeout=2)
+    except asyncio.TimeoutError: 
+        print("Operation timed out")
+        task.cancel() 
+        await task 
+    
+if __name__ == "__main__": 
+    asyncio.run(main())
